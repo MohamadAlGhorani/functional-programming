63256163654710db88c2a6d6ad5e3d9f19eb2146
MohamadAlGhorani <moh.gh266@gmail.com>
2019-11-07 15:15:27 +0100

    clean data opdracht

---
+++ cleaning-data/clean.js
@@ -0,0 +1,27 @@
+(function request() {
+    fetch("enquete.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            getUglyData(data);
+        });
+})();
+
+function getUglyData(jsonObj) {
+    let data = jsonObj
+        .map(item => item["Kleur haar (HEX code)"]);
+    niceData(data);
+}
+
+function niceData(data) {
+    let newData = data
+        .map(item => item
+            .toString()
+            .toUpperCase()
+        )
+        .filter(item => item !== "" && (/^((0x){0,1}|#{0,1})([0-9A-F]{8}|[0-9A-F]{6})$/).test(item))
+        .map((char) => char.charAt(0) !== "#" ? item = "#" + char : char);
+
+    console.log(newData)
+}
