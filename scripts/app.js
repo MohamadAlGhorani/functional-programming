803ab0510a8096397d066eb88b57cc76b598f172
MohamadAlGhorani <moh.gh266@gmail.com>
2019-11-08 13:37:23 +0100

    data ophalen

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 const eindpoint = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-22/sparql";
-
+const categorieLength = 19;
 const categorieQuery = `
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
@@ -32,15 +32,15 @@ objArray = [];
 
 function loopData(data) {
     data.map(item => {
-        const newproperty = {
+        const newProperties = {
             category: ` <${item.category.value}>`,
             categoryLabel: item.categoryLabel.value,
             countObj: item.choCount.value
         }
-        objArray.push(newproperty)
+        objArray.push(newProperties)
     })
 
-    if (objArray.length == 19) {
+    if (objArray.length == categorieLength) {
         console.log(objArray);
         objArray.forEach(item => {
             const geoQuery = `
@@ -52,13 +52,12 @@ function loopData(data) {
             PREFIX foaf: <http://xmlns.com/foaf/0.1/>
             # tel aantallen per materiaal
             SELECT ?subcategorie ?geoLabel (COUNT(?cho) AS ?choCount) WHERE {
-              # haal van een term in de thesaurus de subcategorieen op
+             # haal van een term in de thesaurus de subcategorieen op
              ${item.category} skos:narrower* ?subcategorie .
-              # haal de objecten van deze subcategorieen en het geo
+             # haal de objecten van deze subcategorieen en het materiaal
              ?cho edm:isRelatedTo ?subcategorie .
-             ?cho dct:spatial ?geo.
-              #<https://hdl.handle.net/20.500.11840/termmaster2> skos:narrower ?geo .
-              # haal het Label op van geo
+             ?cho dct:spatial ?geo .
+             # haal het Label op van materiaal
              ?geo skos:prefLabel ?geoLabel .
             }
             GROUP BY ?subcategorie ?geoLabel
@@ -71,8 +70,14 @@ function loopData(data) {
 geoArray = [];
 
 function handelGeo(data) {
-    geoArray.push(data)
-    if (geoArray.length == 19) {
+    const test = data.map(item => {
+        return {
+            gebiedLabel: item.geoLabel.value,
+            aantalObjInGebied: item.choCount.value
+        }
+    })
+    geoArray.push(test)
+    if (geoArray.length == categorieLength) {
         console.log(geoArray)
     }
 }
